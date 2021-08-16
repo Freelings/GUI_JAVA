a4f36a593cce730813d326fa5dc9f2be791c079c
Freelings 2020 <jianglang2017@163.com>
2021-08-16 23:09:02 +0800

    update

--- GUI/Componenet/FileChooser/FileChooser.java
+++ GUI/Componenet/FileChooser/FileChooser.java
@@ -15,7 +15,10 @@ public class FileChooser extends JFrame implements ActionListener{
 	 */
 	private static final long serialVersionUID = 3812858559328873549L;
 	
-	JButton open=null;  
+	JButton open = null;
+	String folderChoosed = null;
+	String fileChoosed = null;
+	
 
     public FileChooser(){  
         open=new JButton("open");  
@@ -27,21 +30,28 @@ public class FileChooser extends JFrame implements ActionListener{
     }  
 
     @Override  
-    public void actionPerformed(ActionEvent e) {  
-
+    public void actionPerformed(ActionEvent e) {
         JFileChooser jfc = new JFileChooser();  
         jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES );  
         jfc.showDialog(new JLabel(), "Choose");  
         File file=jfc.getSelectedFile();  
         if(file.isDirectory()){  
+        	fileChoosed = file.getAbsolutePath();
             System.out.println("Folder: " + file.getAbsolutePath());  
-        }else if(file.isFile()){  
+        }else if(file.isFile()){
+        	fileChoosed = file.getAbsolutePath();
             System.out.println("File: " + file.getAbsolutePath());  
-        }  
+        }
+        fileChoosed = jfc.getSelectedFile().getName();
         System.out.println(jfc.getSelectedFile().getName());  
           
-    }  
-    
+    }
 
-  
+	public String getFolderChoosed() {
+		return folderChoosed;
+	}
+
+	public String getFileChoosed() {
+		return fileChoosed;
+	}  
 }
--- GUI/appClientModule/Appplication/Main.java
+++ GUI/appClientModule/Appplication/Main.java
@@ -5,6 +5,9 @@ import FileChooser.FileChooser;
 public class Main {
 	
 	public static void main(String[] args) {
-		new FileChooser();
+		FileChooser fileChosed = new FileChooser();
+		System.out.println("*************************");
+		System.out.println(fileChosed.getFolderChoosed());
+		System.out.println(fileChosed.getFileChoosed()); 
 	}
 }
